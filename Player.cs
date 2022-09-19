4dd9e3158a777013da7ca9530f2e5cf6e237dd87
MarcusSakae <82marsak@skola.boras.se>
2022-09-19 11:34:08 +0200

    various

--- Player.cs
+++ Player.cs
@@ -2,7 +2,8 @@
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
-    public bool NameSelected { get; set; }
+    public bool HasName { get; set; }
+    public bool HasPosition { get; set; }
     public int X { get; set; }
     public int Y { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -4,26 +4,37 @@ using System.Text;
 
 internal class Program
 {
+
+
     private static async Task Main(string[] args)
     {
         using Socket client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 11122);
         Player player = new();
+
         while (true)
         {
-            // Send message.
-            var message = "Hi friends 👋!<|EOM|>";
-            var messageBytes = Encoding.UTF8.GetBytes(message);
-            _ = await client.SendAsync(messageBytes, SocketFlags.None);
-            Console.WriteLine($"Socket client sent message: \"{message}\"");
+            string message = "";
+            if (!player.HasPosition)
+            {
+                message = TcpMessage.GET_POSITION;
+            }
 
-
-            Thread.Sleep(2000);
+            if (message.Length > 0)
+            {
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+            }
+            Thread.Sleep(1000);
         }
-
         client.Shutdown(SocketShutdown.Both);
-
     }
 
+}
 
+
+class TcpMessage
+{
+    public const string GET_POSITION = "GET_POSITION";
 }
